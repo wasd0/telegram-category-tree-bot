16d3cdb594da8ba3e9cc192462f07f853ca5c7fd
wasd <kirshvedov13@gmail.com>
2023-12-12 21:45:41 +0200

    init project

--- src/main/java/com/wasd/categorytreebot/CategoryTreeBotApplication.java
+++ src/main/java/com/wasd/categorytreebot/CategoryTreeBotApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CategoryTreeBotApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CategoryTreeBotApplication.class, args);
     }
-
 }
---
+++ src/main/java/com/wasd/categorytreebot/config/TelegramBotConfig.java
@@ -0,0 +1,31 @@
+package com.wasd.categorytreebot.config;
+
+import com.wasd.categorytreebot.bot.CategoryBot;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Configuration
+@PropertySource("classpath:properties/bot.properties")
+public class TelegramBotConfig {
+    @Value("${categoryBot.username}")
+    private String categoryBotUsername;
+    @Value("${categoryBot.token}")
+    private String categoryBotToken;
+    
+    @Bean
+    public TelegramBotsApi telegramBotsApi() throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(categoryBot());
+        return telegramBotsApi;
+    }
+    
+    @Bean
+    public CategoryBot categoryBot() {
+        return new CategoryBot(categoryBotToken, categoryBotUsername);
+    }
+}
