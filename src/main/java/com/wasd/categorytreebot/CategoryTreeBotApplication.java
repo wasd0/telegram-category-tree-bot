f0a190aec15c1b1a0102fba259c993efa42e0b84
wasd <kirshvedov13@gmail.com>
2023-12-12 16:23:02 +0200

    init project

---
+++ src/main/java/com/wasd/categorytreebot/CategoryTreeBotApplication.java
@@ -0,0 +1,13 @@
+package com.wasd.categorytreebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryTreeBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CategoryTreeBotApplication.class, args);
+    }
+
+}
