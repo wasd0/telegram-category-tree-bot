f0e33d525d25d0414d249044db7ecce0c3975322
wasd <kirshvedov13@gmail.com>
2023-12-17 23:57:53 +0200

    add excel service deserialization tests

--- src/main/java/com/wasd/categorytreebot/service/application/file/ExcelSerializeService.java
+++ src/main/java/com/wasd/categorytreebot/service/application/file/ExcelSerializeService.java
@@ -1,5 +1,7 @@
 package com.wasd.categorytreebot.service.application.file;
 
+import org.apache.poi.openxml4j.exceptions.NotOfficeXmlFileException;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
@@ -7,5 +9,6 @@ import java.util.Map;
 
 public interface ExcelSerializeService {
     File serialize(String filePath, Map<Integer, List<String>> content) throws IOException, NullPointerException;
-    Map<Integer, List<String>> deserialize(String filePath) throws IOException, NullPointerException;
+
+    Map<Integer, List<String>> deserialize(String filePath) throws NotOfficeXmlFileException, NullPointerException, IOException;
 }
